750a1e8cfcf3302e7c3e7ea33591b391e2f8fc01
Hanami High <luvvzari06@gmail.com>
2026-08-19 10:42:59 -0400

    Load Profile Studio upload compatibility styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import PublicSessionBridge from "./components/PublicSessionBridge";
 import "./globals.css";
 import "./mobile.css";
 import "./accessibility.css";
+import "./profile-studio-overrides.css";
 
 export const metadata: Metadata = {
   title: "Hanami High",
