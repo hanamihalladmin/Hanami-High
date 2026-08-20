a73f8b894cc74c4c7551e5fb624f8a9874899c89
Hanami High <luvvzari06@gmail.com>
2026-08-19 22:37:59 -0400

    Wire roadmap manager into Admin and Owner workspace

--- app/portal/admin/AdminWorkspace.tsx
+++ app/portal/admin/AdminWorkspace.tsx
@@ -16,6 +16,7 @@ import AdminHallPassManager from "./AdminHallPassManager";
 import AdminNarrativeModerationPanel from "./AdminNarrativeModerationPanel";
 import AdminStaffAnalyticsPanel from "./AdminStaffAnalyticsPanel";
 import AdminContentApprovalPanel from "./AdminContentApprovalPanel";
+import AdminRoadmapManager from "./AdminRoadmapManager";
 
 type Access={site_admin:boolean;content_editor:boolean;moderator:boolean};
 
@@ -27,7 +28,7 @@ export default function AdminWorkspace({accessToken,userId,access,ownerMode=fals
   {access.site_admin&&<><AdminGovernancePanel accessToken={accessToken} ownerMode={ownerMode}/><AdminOperationsExpansionPanel accessToken={accessToken} userId={userId}/></>}
   {hasAdminAccess&&<><AdminStaffAnalyticsPanel accessToken={accessToken}/><AdminContentApprovalPanel accessToken={accessToken}/><AdminNarrativeModerationPanel accessToken={accessToken}/><AdminHallPassManager accessToken={accessToken}/></>}
   {(access.site_admin||canModerate)&&<AdminRoleplaySystemsManager accessToken={accessToken} access={access}/>} 
-  {canEditContent&&<><AdminSchoolStatusManager accessToken={accessToken} userId={userId} access={access}/><AdminAnnouncementManager accessToken={accessToken} userId={userId} access={access}/><AdminEventManager accessToken={accessToken} userId={userId} access={access}/><AdminOpportunityManager accessToken={accessToken} userId={userId}/><AdminOfficeRequestManager accessToken={accessToken} userId={userId}/></>}
+  {canEditContent&&<><AdminRoadmapManager accessToken={accessToken} userId={userId}/><AdminSchoolStatusManager accessToken={accessToken} userId={userId} access={access}/><AdminAnnouncementManager accessToken={accessToken} userId={userId} access={access}/><AdminEventManager accessToken={accessToken} userId={userId} access={access}/><AdminOpportunityManager accessToken={accessToken} userId={userId}/><AdminOfficeRequestManager accessToken={accessToken} userId={userId}/></>}
   {canModerate&&<><AdminSupportTicketManager accessToken={accessToken} userId={userId}/><AdminModerationManager accessToken={accessToken} userId={userId} access={access}/><AdminCharacterDirectory accessToken={accessToken}/></>}
   {access.site_admin&&<AdminAcademicManager accessToken={accessToken}/>} 
  </>;
