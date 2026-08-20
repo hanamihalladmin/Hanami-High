2e8482056b55a49bb8e6f952cfe6bca5680d2e0d
Hanami High <luvvzari06@gmail.com>
2026-08-19 21:59:33 -0400

    Add storyline incident and evidence operations to Admin workspace

--- app/portal/admin/AdminWorkspace.tsx
+++ app/portal/admin/AdminWorkspace.tsx
@@ -13,6 +13,7 @@ import AdminOperationsExpansionPanel from "./AdminOperationsExpansionPanel";
 import AdminSupportTicketManager from "./AdminSupportTicketManager";
 import AdminRoleplaySystemsManager from "./AdminRoleplaySystemsManager";
 import AdminHallPassManager from "./AdminHallPassManager";
+import AdminNarrativeModerationPanel from "./AdminNarrativeModerationPanel";
 
 type Access={site_admin:boolean;content_editor:boolean;moderator:boolean};
 
@@ -22,6 +23,7 @@ export default function AdminWorkspace({accessToken,userId,access,ownerMode=fals
  const hasAdminAccess=access.site_admin||access.content_editor||access.moderator;
  return <>
   {access.site_admin&&<><AdminGovernancePanel accessToken={accessToken} ownerMode={ownerMode}/><AdminOperationsExpansionPanel accessToken={accessToken} userId={userId}/></>}
+  {hasAdminAccess&&<AdminNarrativeModerationPanel accessToken={accessToken}/>} 
   {(access.site_admin||canModerate)&&<AdminRoleplaySystemsManager accessToken={accessToken} access={access}/>} 
   {hasAdminAccess&&<AdminHallPassManager accessToken={accessToken}/>} 
   {canEditContent&&<><AdminSchoolStatusManager accessToken={accessToken} userId={userId} access={access}/><AdminAnnouncementManager accessToken={accessToken} userId={userId} access={access}/><AdminEventManager accessToken={accessToken} userId={userId} access={access}/><AdminOpportunityManager accessToken={accessToken} userId={userId}/><AdminOfficeRequestManager accessToken={accessToken} userId={userId}/></>}
