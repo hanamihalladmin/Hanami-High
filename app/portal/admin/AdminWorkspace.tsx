6f5a793231881de0bccc385ff8430e57ebf2ff59
Hanami High <luvvzari06@gmail.com>
2026-08-20 00:07:49 -0400

    Wire roadmap completion operations into Admin and Owner

--- app/portal/admin/AdminWorkspace.tsx
+++ app/portal/admin/AdminWorkspace.tsx
@@ -17,6 +17,7 @@ import AdminNarrativeModerationPanel from "./AdminNarrativeModerationPanel";
 import AdminStaffAnalyticsPanel from "./AdminStaffAnalyticsPanel";
 import AdminContentApprovalPanel from "./AdminContentApprovalPanel";
 import AdminRoadmapManager from "./AdminRoadmapManager";
+import AdminRoadmapOperationsPanel from "./AdminRoadmapOperationsPanel";
 
 type Access={site_admin:boolean;content_editor:boolean;moderator:boolean};
 
@@ -28,7 +29,8 @@ export default function AdminWorkspace({accessToken,userId,access,ownerMode=fals
   {access.site_admin&&<><AdminGovernancePanel accessToken={accessToken} ownerMode={ownerMode}/><AdminOperationsExpansionPanel accessToken={accessToken} userId={userId}/></>}
   {hasAdminAccess&&<><AdminStaffAnalyticsPanel accessToken={accessToken}/><AdminContentApprovalPanel accessToken={accessToken}/><AdminNarrativeModerationPanel accessToken={accessToken}/><AdminHallPassManager accessToken={accessToken}/></>}
   {(access.site_admin||canModerate)&&<AdminRoleplaySystemsManager accessToken={accessToken} access={access}/>} 
-  {canEditContent&&<><AdminRoadmapManager accessToken={accessToken} userId={userId}/><AdminSchoolStatusManager accessToken={accessToken} userId={userId} access={access}/><AdminAnnouncementManager accessToken={accessToken} userId={userId} access={access}/><AdminEventManager accessToken={accessToken} userId={userId} access={access}/><AdminOpportunityManager accessToken={accessToken} userId={userId}/><AdminOfficeRequestManager accessToken={accessToken} userId={userId}/></>}
+  {canEditContent&&<><AdminRoadmapManager accessToken={accessToken} userId={userId}/><AdminRoadmapOperationsPanel accessToken={accessToken} userId={userId} canModerate={canModerate}/><AdminSchoolStatusManager accessToken={accessToken} userId={userId} access={access}/><AdminAnnouncementManager accessToken={accessToken} userId={userId} access={access}/><AdminEventManager accessToken={accessToken} userId={userId} access={access}/><AdminOpportunityManager accessToken={accessToken} userId={userId}/><AdminOfficeRequestManager accessToken={accessToken} userId={userId}/></>}
+  {!canEditContent&&canModerate&&<AdminRoadmapOperationsPanel accessToken={accessToken} userId={userId} canModerate/>}
   {canModerate&&<><AdminSupportTicketManager accessToken={accessToken} userId={userId}/><AdminModerationManager accessToken={accessToken} userId={userId} access={access}/><AdminCharacterDirectory accessToken={accessToken}/></>}
   {access.site_admin&&<AdminAcademicManager accessToken={accessToken}/>} 
  </>;
