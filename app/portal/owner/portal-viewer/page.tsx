ac054d9f489de31fa8fcb6f8244089490bd36128
Hanami High <luvvzari06@gmail.com>
2026-08-21 08:32:16 -0400

    Add Owner all-user portal viewer route

---
+++ app/portal/owner/portal-viewer/page.tsx
@@ -0,0 +1,4 @@
+import OwnerUserPortalViewer from "./OwnerUserPortalViewer";
+import styles from "../../PortalAppPage.module.css";
+
+export default function OwnerPortalViewerPage(){return <main className={styles.page}><OwnerUserPortalViewer/></main>;}
