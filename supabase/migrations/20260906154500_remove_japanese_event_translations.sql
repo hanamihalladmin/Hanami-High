a48ad1010a3aacbb8241cca2d70b190bcd1587c2
Hanami High <luvvzari06@gmail.com>
2026-09-06 11:55:21 -0400

    Record Japanese event title cleanup

---
+++ supabase/migrations/20260906154500_remove_japanese_event_translations.sql
@@ -0,0 +1,4 @@
+update public.school_calendar_events set title='Greenery Day' where title='Greenery Day (みどりの日)';
+update public.school_calendar_events set title='Constitution Memorial Day' where title='Constitution Memorial Day (憲法記念日)';
+update public.school_calendar_events set title='Citizen’s Holiday' where title='Citizen’s Holiday (国民の休日)';
+update public.school_calendar_events set title='Children’s Day' where title='Children’s Day (こどもの日)';
