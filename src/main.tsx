64c9a2af4a2422dbef6bc9a5bf403d888c0778fa
Hanami High <luvvzari06@gmail.com>
2026-09-07 19:01:40 -0400

    Load Discord shell polish overrides

--- src/main.tsx
+++ src/main.tsx
@@ -46,6 +46,7 @@ import './styles/school-administration.css'
 import './styles/access-modes.css'
 import './styles/early-web-site.css'
 import './styles/discord-shell.css'
+import './styles/discord-shell-polish.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
