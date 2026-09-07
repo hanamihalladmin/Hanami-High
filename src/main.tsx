14cd638deb9caa8991f8700c296618e59efda778
Hanami High <luvvzari06@gmail.com>
2026-09-07 19:41:35 -0400

    Load current Discord Profile Studio overrides

--- src/main.tsx
+++ src/main.tsx
@@ -49,6 +49,7 @@ import './styles/discord-shell.css'
 import './styles/discord-shell-polish.css'
 import './styles/home-discord-channel.css'
 import './styles/discord-profile-experience.css'
+import './styles/discord-profile-studio-current.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
